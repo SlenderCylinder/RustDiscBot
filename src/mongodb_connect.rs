683e642498fbf9a6c38a6db0bf800e67f39769eb
Chamith <chamith.wakista@wfp.org>
2023-10-13 06:01:49 +0000

    More commands added

--- src/mongodb_connect.rs
+++ src/mongodb_connect.rs
@@ -3,10 +3,11 @@ use mongodb::{Client, options::ClientOptions};
 // Define a function to create and return a MongoDB client
 pub async fn get_mongodb_client() -> Result<Client, mongodb::error::Error> {
     // Configure MongoDB client options
-    let client_options = ClientOptions::parse("mongodb+srv://chamithwakista:HpfoBN26dN72uTSM@cluster0.pqapgw1.mongodb.net/discord?retryWrites=true&w=majority").await?;
+    // Retrieve the MongoDB URI from the environment variable
+    let mongodb_uri = env::var("MONGODB_URI").expect("MONGODB_URI not set in .env file");
 
-    // You can specify additional options here, such as authentication
-
+    // Configure MongoDB client options
+    let client_options = ClientOptions::parse(&mongodb_uri).await?;
     // Create and return the MongoDB client
     let client = Client::with_options(client_options)?;
 
